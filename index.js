21105a325f253f67246e8e455484a716fed5731e
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-03-28 19:06:15 +0600

    updated index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 
 
+let notes = [{
+        id: 1,
+        title: "Note title 1",
+        description: "Note description 1"
+    },
+    {
+        id: 2,
+        title: "Note title 2",
+        description: "Note description 2"
+    }
+]
+
 // create route
 app.get('/', (req, res) => {
     res.send('I am form node js');
@@ -11,6 +23,27 @@ app.get('/test/:name', (req, res) => {
     const name = req.params.name;
     res.send(`Hello ${name}`);
 });
+//notes all notes
+app.get('/notes', (req, res) => {
+    if (notes.length == 0) {
+        return res.send("No note found or yet not created");
+    }
+
+    res.send(notes);
+});
+//notes single note
+app.get('/notes/:noteId', (req, res) => {
+
+    const noteId = parseInt(req.params.noteId);
+    const note = notes.find(note => note.id === noteId);
+    if (note) {
+        res.send(note);
+    } else {
+        res.send("Note not found");
+    }
+
+});
+
 app.get('*', (req, res) => {
     res.send('404 Not Found');
 });
